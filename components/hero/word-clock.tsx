4d1af305900eab7654ff7aed8b60b3844db17ef0
Tej Pratap Singh <tejpratap46@gmail.com>
2023-01-04 16:37:27 +0530

    updated word clock

--- components/hero/word-clock.tsx
+++ components/hero/word-clock.tsx
@@ -28,27 +28,48 @@ const WordClock = () => {
 					<span>IT</span>
 					<span className='text-gray-400 dark:text-gray-700'>LAS</span>
 					<span>IS</span>
-					<span className='text-gray-400 dark:text-gray-700'>LAJDBA</span>
+					<span className='text-gray-400 dark:text-gray-700'>WQEWQEITITLAWQEITJDBWQEITA</span>
 				</h1>
 				<h1 className="text-6xl mt-4">
-					<span className='text-gray-400 dark:text-gray-700'>WQEIT</span>
-					<span>{toWords.convert(parseInt(currentMinute)).split(' ').join('L')}</span>
-					<span className='text-gray-400 dark:text-gray-700'>AWLKH</span>
+					<span className='text-gray-400 dark:text-gray-700'>WQT</span>
+					{
+						toWords.convert(parseInt(currentMinute)).split(' ').map((word, i) => {
+							return <span key={i}>
+								<span className='uppercase'>{word}</span>
+								<span className='text-gray-400 dark:text-gray-700'>L</span>
+							</span>
+						})
+					}
+					<span className='text-gray-400 dark:text-gray-700'>WQWQEITEITAWQEITWQEITWLKH</span>
 				</h1>
 				<h1 className="text-6xl mt-4">
 					<span>PAST</span>
 					<span className='text-gray-400 dark:text-gray-700'>WQEIT</span>
-					<span className='text-gray-400 dark:text-gray-700'>AWLKH</span>
+					<span className='text-gray-400 dark:text-gray-700'>AWWQEWQEITITLKHWQEIT</span>
 				</h1>
 				<h1 className="text-6xl">
 					<span className='text-gray-400 dark:text-gray-700'>WQEIT</span>
-					<span>{toWords.convert(parseInt(currentHr))}</span>
-					<span className='text-gray-400 dark:text-gray-700'>AWLKH</span>
+					{
+						toWords.convert(parseInt(currentHr)).split(' ').map((word, i) => {
+							return <span key={i}>
+								<span className='uppercase'>{word}</span>
+								<span className='text-gray-400 dark:text-gray-700'>L</span>
+							</span>
+						})
+					}
+					<span className='text-gray-400 dark:text-gray-700'>WQEITAWQEITWLKWQEITWQEITH</span>
 				</h1>
 				<h1 className="text-6xl mt-4">
 					<span className='text-gray-400 dark:text-gray-700'>QE</span>
-					{toWords.convert(parseInt(currentSecond))}
-					<span className='text-gray-400 dark:text-gray-700'>WT</span>
+					{
+						toWords.convert(parseInt(currentSecond)).split(' ').map((word, i) => {
+							return <span key={i}>
+								<span className='uppercase'>{word}</span>
+								<span className='text-gray-400 dark:text-gray-700'>L</span>
+							</span>
+						})
+					}
+					<span className='text-gray-400 dark:text-gray-700'>WWQEIWQEITTTAWWQEITLKH</span>
 				</h1>
 			</div>
 		</div>
