3d199d205cd4e8a3439b7589a136021330d3fd25
Tej Pratap Singh <tejpratap46@gmail.com>
2024-10-29 09:59:20 +0530

    added more animation for pw

--- components/hero/pixel-watch.tsx
+++ components/hero/pixel-watch.tsx
@@ -11,13 +11,13 @@ const ConcentricRotatingWatchface = () => {
 
 	useEffect(() => {
 		const timer = setInterval(() => {
-			time.setSeconds(time.getSeconds() + animatedTick * 10)
-			time.setMinutes(time.getSeconds() + animatedTick * 10)
+			time.setSeconds(time.getSeconds() + animatedTick)
+			time.setMinutes(time.getSeconds() + animatedTick)
 			if (animatedTick % 2 == 0) {
-				time.setHours(time.getSeconds() + animatedTick / 2)
+				time.setHours(time.getSeconds() + (animatedTick / 2))
 			}
 
-			if (animatedTick >= 6) {
+			if (animatedTick >= 60) {
 				clearInterval(timer)
 				setShouldStartClock(true)
 				setTime(new Date())
